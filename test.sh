3fe275cdd67e4c5a48a8567bf3d4db1f044299df
Megax <megax@yeahunter.hu>
2013-07-22 17:38:28 +0200

    * Test.sh fix.

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 xbuild /p:Configuration="Debug" CsharpSQLite.sln /flp:LogFile=xbuild.log;Verbosity=Detailed
 
 cd Run/Tests/Debug
-/usr/bin/nunit-console System.Data.SQLite.Tests.dll
+nunit-console System.Data.SQLite.Tests.dll
